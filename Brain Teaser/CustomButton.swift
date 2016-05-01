72e9e69ff6f8ffd3f5c9f4b31b7934422dc13be3
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-05-01 09:38:59 -0700

    feat: Added custom button class

---
+++ Brain Teaser/CustomButton.swift
@@ -0,0 +1,41 @@
+//
+//  CustomButton.swift
+//  Brain Teaser
+//
+//  Created by Mikael Mukhsikaroyan on 5/1/16.
+//  Copyright © 2016 MSquared. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    @IBInspectable var fontColor: UIColor = UIColor.blackColor() {
+        didSet {
+            self.tintColor = fontColor
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+        
+    }
+    
+}
