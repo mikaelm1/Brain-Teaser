219b6efcd7e93dac46eaeedd95c52807f08bf4a4
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-05-01 09:56:26 -0700

    feat: Added pop animations to custom button class

--- Brain Teaser/CustomButton.swift
+++ Brain Teaser/CustomButton.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import pop
 
 @IBDesignable
 class CustomButton: UIButton {
@@ -36,6 +37,40 @@ class CustomButton: UIButton {
     func setupView() {
         self.layer.cornerRadius = cornerRadius
         
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), forControlEvents: .TouchDown)
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), forControlEvents: .TouchDragEnter)
+        
+        self.addTarget(self, action: #selector(CustomButton.scaleAnimation), forControlEvents: .TouchUpInside)
+        self.addTarget(self, action: #selector(CustomButton.scaleDefault), forControlEvents: .TouchDragExit)
+    }
+    
+    func scaleToSmall() {
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        // We will be slightly scaling in the animation to 95% of original size
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(0.95, 0.95))
+        // The key is for us in case we later we want to end the animation or do something else with it. The key could be anything
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSmallAnimation")
+    }
+    
+    func scaleAnimation() {
+        let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)
+        // The velocity is how fast it will spring out and back into place
+        scaleAnim.velocity  = NSValue(CGSize: CGSizeMake(3.0, 3.0))
+        // After the spring animation, we want it back to normal
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(1.0, 1.0))
+        scaleAnim.springBounciness = 18
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSpringAnimation")
+    }
+    
+    func scaleDefault() {
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(1.0, 1.0))
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleDefaultAnimation")
     }
     
 }
+
+
+
+
+
